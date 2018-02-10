890384821c24df7922b91de51a63ed672e3966a1
gjw199513@163.com <gjw605134015>
2018-02-10 18:55:24 +0800

    模板+web表单+数据库+用户登录++用户信息页和头像+单元测试+关注者、联系人和好友

--- app/models.py
+++ app/models.py
@@ -7,6 +7,12 @@ __date__ = '2018/2/7 9:44'
 from app import db
 from hashlib import md5
 
+# 添加followers表
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+                     )
+
 
 # 用户类
 class User(db.Model):
@@ -18,6 +24,18 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+    # 关注
+    followed = db.relationship('User',
+                               # 指定关系的辅助表
+                               secondary=followers,
+                               # 辅助表中连接左边实体(发起关注的用户)的条件
+                               primaryjoin=(followers.c.follower_id == id),
+                               # 辅助表中连接右边实体(被关注的用户)的条件
+                               secondaryjoin=(followers.c.follower_id == id),
+                               # 定义这种关系将如何从右边实体进行访问
+                               backref=db.backref('followers', lazy='dynamic'),
+                               lazy='dynamic')
+
     # Flask-Login 扩展需要在我们的 User 类中实现一些特定的方法。
     # 但是类如何去实现这些方法却没有什么要求。
 
@@ -56,11 +74,33 @@ class User(db.Model):
             return nickname
         version = 2
         while True:
-            new_nickname = nickname+str(version)
+            new_nickname = nickname + str(version)
             if User.query.filter_by(nickname=new_nickname).first():
                 break
             version += 1
         return new_nickname
+
+    # 关注
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+            return self
+
+    # 取消关注
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+            return self
+
+    # 是否关注
+    def is_following(self, user):
+        return self.followed.filter(followers.c.follower_id == user.id).count() > 0
+
+    # 查询被关注者的blog
+    def followed_posts(self):
+        # 连接、过滤、排序
+        return Post.query.join(followers, (followers.c.follower_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id).order_by(Post.timestamp.desc())
 
     # 打印这个类的方式
     def __repr__(self):
